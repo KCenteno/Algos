a614eea3c9ae2565f6651514db457bb45f47acf8
Kevin Centeno <KevinCenteno_90@yahoo.com>
2022-11-04 12:04:06 -0400

    commit

--- SlidingWindow/longestSubstringWithoutRepeatingCharacters.py
+++ SlidingWindow/longestSubstringWithoutRepeatingCharacters.py
@@ -1,12 +1,12 @@
 def lengthOfLongestSubstring(s):
     hashset = set()
-    l, r, res = 0, 0, 0
-    for r in range(len(s)):
+    l, r, res = 0, 0, 0 #initialize pointers
+    for r in range(len(s)): # loop throught the string
         while s[r] in hashset: # checking for dupicates
-            hashset.remove(s[l])
-            l += 1
+            hashset.remove(s[l]) # do something
+            l += 1  # update the pointer
         hashset.add(s[r])
-        res = max(res, r - l + 1)
+        res = max(res, r - l + 1) # find res
     return res
 
 print(lengthOfLongestSubstring("abcabcbb"))
